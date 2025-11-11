bc52e9ad57936ef4e75fc7c11fe2546d424ea4b3
johannbleza <johann.bleza@icloud.com>
2025-11-11 23:00:52 +0800

    Implement sidebar component

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,62 @@
+import { Package, Printer, ShirtIcon, Users } from "lucide-react";
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+
+// Menu items.
+const items = [
+  {
+    title: "Orders",
+    url: "#",
+    icon: Package,
+  },
+  {
+    title: "Buyers",
+    url: "#",
+    icon: Users,
+  },
+  {
+    title: "Items",
+    url: "#",
+    icon: ShirtIcon,
+  },
+  {
+    title: "Print",
+    url: "#",
+    icon: Printer,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>BZA Dashboard</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
